007a2bbdaaf2e5e8ec93779cfb962baa8a37eab8
thumperward <thumperward@hotmail.com>
2023-07-06 16:01:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Multipass installer
+
+This script will install Multipass from scratch on MacOS.
+
+## Prerequisites
+
+- An SSH key in ed25519 format, added to your keyring.
+- `$HOME/.local/bin` should be in `$PATH` (note: ensure you use `$HOME` and not
+  `~` as `which` and other tooling won't expand the tilde).
+- Visual Studio Code (not needed for the install, but needed to make use of
+  it).
+
+## Usage
+
+```sh
+./install.sh
+```
+
+Enter your password when prompted.
+
+Now just clone a repository locally container a devcontainer configuration and
+select `Clone in volume` (NOTE: NOT `Reopen in Container`) to open it in the
+Docker VM.
+
+## License
+
+(C) CTS 2023
+
+MIT License, see [LICENSE](LICENSE) for details.
---
+++ install.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# Install Multipass.
+curl -o multipass.pkg -SL https://github.com/canonical/multipass/releases/download/v1.12.1/multipass-1.12.1+mac-Darwin.pkg
+sudo installer -pkg multipass.pkg -target /
+rm multipass.pkg
+
+# Install the Docker CLI and helpers.
+CPU=$(uname -m)
+case $CPU in
+  arm64) CPU=aarch64 ;;
+esac
+curl -o docker.tgz -SL https://download.docker.com/mac/static/stable/$CPU/docker-20.10.10.tgz
+tar xzvf docker.tgz
+install docker/docker ~/.local/bin/
+install docker/cli-plugins/docker-buildx ~/.local/bin/
+install docker/cli-plugins/docker-app ~/.local/bin/
+(
+  cd ~/.docker/cli-plugins
+  ln -s ~/.local/bin/docker-buildx
+  ln -s ~/.local/bin/docker-app
+)
+rm -rf docker.tgz docker/
+
+# Start the prepackaged Docker VM and set it as the default machine.
+multipass launch docker
+multipass set client.primary-name=docker
+
+# Copy the local user's SSH key to the VM.
+cat ~/.ssh/id_ed25519.pub | multipass exec docker -- bash -c 'cat -- >> .ssh/authorized_keys'
+
+# Add SSH configuration for the local Docker VM.
+VM_IP=$(multipass ls --format json | jq -r  '.list[].ipv4[0]')
+cat <<EOF >>~/.ssh/config
+Host docker.local
+  HostName $VM_IP
+  User ubuntu
+EOF
+
+# Add a Docker context pointing to the VM and select it.
+docker context create ssh --docker "host=ssh://docker.local"
+docker context use ssh
