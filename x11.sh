430e561a529aad5daa5fdc2b7044278f36c5463b
chriscunningham-cts <chris.cunningham@cts.co>
2024-10-03 09:54:40 +0000

    don't hardcode IPs

--- x11.sh
+++ x11.sh
@@ -5,5 +5,6 @@
 # 192.168.61.1 is the local machine.
 # Disable authentication and enable nework connections in XQuartz.
 
-xhost 192.168.64.10
-docker run -e DISPLAY=192.168.64.1:0 jess/firefox
+open /Applications/Utilities/XQuartz.app
+xhost "$(multipass ls --format csv | grep docker | cut -d ',' -f 3)"                          # 192.168.64.10
+docker run -e DISPLAY="$(ifconfig bridge100 | grep 'inet ' | cut -f 2 -d ' '):0" jess/firefox # 192.168.61.1
