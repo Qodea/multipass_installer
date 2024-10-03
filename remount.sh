a5e71dfc68aa5d051f28fe19af2ec68d74a490a9
chriscunningham-cts <chris.cunningham@cts.co>
2024-10-03 10:27:31 +0000

    non-dummy developer directory name

--- remount.sh
+++ remount.sh
@@ -2,6 +2,6 @@
 
 multipass stop docker
 multipass umount docker:"$HOME"
-multipass mount --type native "$HOME/my_developer_dir" docker
+multipass mount --type native "$HOME/Developer" docker
 # /my_developer_dir would contain all your devcontainer-supported repositories
 multipass start docker
